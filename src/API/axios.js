03a163075e2ad27962ef5e3969217dcd9528734b
Michael Dillahunty <michaeldillahunty01@gmail.com>
2023-12-11 20:06:51 -0600

    test

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with custom configuration
 const instance =  axios.create({
-  baseURL: 'http://localhost:8000', // Replace with your API's base URL
+  baseURL: 'http://18.217.120.233', // Replace with your API's base URL
  
   timeout: 10000, // Set a timeout for requests (in milliseconds)
   
