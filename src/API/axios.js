c972d89f16583bca52ef0c92380344e9d50bcaa4
Michael Dillahunty <michaeldillahunty01@gmail.com>
2023-12-12 13:22:14 -0600

    update axios baseURL

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with custom configuration
 const instance =  axios.create({
-  baseURL: 'ec2-18-217-120-233.us-east-2.compute.amazonaws.com', // Replace with your API's base URL
+  baseURL: 'https://backend-mn36itr6dq-uc.a.run.app', // Replace with your API's base URL
  
   timeout: 10000, // Set a timeout for requests (in milliseconds)
   
